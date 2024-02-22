1ad708b664ad375b7522d3f4655f9b2f05c36e0e
bavinclint <bavinclint@gmail.com>
2024-02-22 16:47:23 +0300

    added navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
-import '@/assets/styles/globals.css'
+import Navbar from '@/components/Navbar';
+import '@/assets/styles/globals.css';
 
 export const metadata = {
   title: 'PropertyPulse | Find the Perfect Rental',
@@ -12,7 +13,8 @@ const MainLayout = ({ children}) => {
   return (
     <html lang='en'>
         <body>
-            <div>{children}</div>   
+          <Navbar />
+            <main>{children}</main>   
         </body>
     </html>
   )
