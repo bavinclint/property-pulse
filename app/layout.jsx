a11f08aee4093e0e7e873cfae86ee50aaec88917
bavinclint <bavinclint@gmail.com>
2024-02-22 12:44:29 +0300

    Created layout, homepage and styles

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import '@/assets/styles/globals.css'
+
+export const metadata = {
+  title: 'PropertyPulse | Find the Perfect Rental',
+  // description: 'Find your dream home with Property Pulse, the ultimate rental property search engine. Browse through thousands of properties and find the perfect one for you.',
+  description: 'Find your dream rental home with PropertyPulse, a comprehensive real estate search engine that helps you find the perfect place to live or invest in.',
+  keywords: 'rental, find rentals, find properties'
+
+}
+
+const MainLayout = ({ children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div>{children}</div>   
+        </body>
+    </html>
+  )
+}
+
+export default MainLayout
