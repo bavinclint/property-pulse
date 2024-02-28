a79f489f6034b542a109a1a08752bf539fec442d
bavinclint <bavinclint@gmail.com>
2024-02-28 14:13:22 +0300

    Session Provider Wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 import "@/assets/styles/globals.css";
 
 export const metadata = {
@@ -12,13 +13,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
