0e6e0ae5c0f8a02f3ccfd16f8ebf058482df420b
bavinclint <bavinclint@gmail.com>
2024-03-19 18:56:24 +0300

    updated files

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,32 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import AuthProvider from "@/components/AuthProvider";
-import "@/assets/styles/globals.css";
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
+import { ToastContainer } from 'react-toastify';
+import { GlobalProvider } from '@/context/GlobalContext';
+import '@/assets/styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+import 'photoswipe/dist/photoswipe.css';
 
 export const metadata = {
-  title: "PropertyPulse | Find the Perfect Rental",
-  // description: 'Find your dream home with Property Pulse, the ultimate rental property search engine. Browse through thousands of properties and find the perfect one for you.',
-  description:
-    "Find your dream rental home with PropertyPulse, a comprehensive real estate search engine that helps you find the perfect place to live or invest in.",
-  keywords: "rental, find rentals, find properties",
+  title: 'PropertyPulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
 };
 
 const MainLayout = ({ children }) => {
   return (
-    <AuthProvider>
-      <html lang="en">
-        <body>
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
-        </body>
-      </html>
-    </AuthProvider>
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang='en'>
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   );
 };
-
 export default MainLayout;
